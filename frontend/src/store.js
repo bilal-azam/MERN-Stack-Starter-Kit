e6de2aee522f4e15070154275b6e971b8659ac50
Bilal Azam <bilal.azam1@outlook.com>
2023-03-04 00:00:00 +0000

    Set up Redux store and counter slice in frontend

---
+++ frontend/src/store.js
@@ -0,0 +1,30 @@
+// frontend/src/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './features/counter/counterSlice';
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
+// frontend/src/features/counter/counterSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+export default counterSlice.reducer;
