df43c517e2961bcec8ea6e5cad800977e978b2b1
Bilal Azam <bilal.azam1@outlook.com>
2023-03-07 00:00:00 +0000

    Set up Redis caching in backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 // backend/index.js
 const express = require('express');
 const mongoose = require('mongoose');
+const redis = require('redis');
+const redisClient = redis.createClient();
+
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true });
+redisClient.on('error', (err) => console.log('Redis error: ', err));
 
 app.use(express.json());
+
+app.get('/cache', (req, res) => {
+  redisClient.get('key', (err, data) => {
+    if (err) throw err;
+    res.send(data);
+  });
+});
+
 app.listen(5000, () => console.log('Server running on port 5000'));
