becdb2aae9a527f4624f647a926cb9d61849d18a
Bilal Azam <bilal.azam1@outlook.com>
2023-03-08 00:00:00 +0000

    Implement user authentication endpoints in backend

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const redis = require('redis');
-const redisClient = redis.createClient();
+const jwt = require('jsonwebtoken');
+const User = require('./models/User');
 
 const app = express();
+const redisClient = redis.createClient();
 
 mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true });
 redisClient.on('error', (err) => console.log('Redis error: ', err));
@@ -16,6 +18,22 @@ app.get('/cache', (req, res) => {
     if (err) throw err;
     res.send(data);
   });
+});
+
+// Register endpoint
+app.post('/register', async (req, res) => {
+  const user = new User(req.body);
+  await user.save();
+  res.status(201).send(user);
+});
+
+// Login endpoint
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email, password });
+  if (!user) return res.status(401).send('Invalid credentials');
+  const token = jwt.sign({ id: user._id }, 'secret', { expiresIn: '1h' });
+  res.send({ token });
 });
 
 app.listen(5000, () => console.log('Server running on port 5000'));
