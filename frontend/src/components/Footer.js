9bedfa1c667d5911b774e5b2964fbc7b6a7495ce
Bilal Azam <bilal.azam1@outlook.com>
2023-03-11 00:00:00 +0000

    Create basic UI components: Header and Footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Footer() {
+  return <footer><p>&copy; 2024 MyFullStackApp</p></footer>;
+}
+
+export default Footer;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Header() {
+  return <header><h1>MyFullStackApp</h1></header>;
+}
+
+export default Header;
