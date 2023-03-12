14ebce1409d39c490cba207292d573a286c10b97
Bilal Azam <bilal.azam1@outlook.com>
2023-03-12 00:00:00 +0000

    Enhance backend API with user CRUD operations

---
+++ backend/models/User.js
@@ -0,0 +1,9 @@
+// backend/models/User.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+module.exports = mongoose.model('User', userSchema);
