307e13f10237f2f5588eb8bb1d19817876b76d65
Bilal Azam <bilal.azam1@outlook.com>
2023-03-17 00:00:00 +0000

    Enhance Redux slice with async login action

--- frontend/src/features/user/userSlice.js
+++ frontend/src/features/user/userSlice.js
@@ -1,14 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit';
+// frontend/src/features/user/userSlice.js (update with async logic)
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { login } from '../../auth';
+
+export const loginUser = createAsyncThunk('user/loginUser', async (credentials) => {
+  const response = await login(credentials);
+  return response.token;
+});
 
 export const userSlice = createSlice({
   name: 'user',
   initialState: { token: null },
   reducers: {
-    setToken: (state, action) => {
-      state.token = action.payload;
+    logout: (state) => {
+      state.token = null;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(loginUser.fulfilled, (state, action) => {
+      state.token = action.payload;
+    });
   },
 });
 
-export const { setToken } = userSlice.actions;
+export const { logout } = userSlice.actions;
 export default userSlice.reducer;
