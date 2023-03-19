0d918f9e5b5ccfcd1d46634cf95a10836fa8fc4a
Bilal Azam <bilal.azam1@outlook.com>
2023-03-19 00:00:00 +0000

    Optimize frontend with code splitting and lazy loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+// frontend/src/App.js (update for code splitting)
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Login from './Login';
+
+const Login = lazy(() => import('./Login'));
 
 function App() {
   return (
     <Router>
       <Header />
-      <Switch>
-        <Route path="/login" component={Login} />
-        {/* Add other routes here */}
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path="/login" component={Login} />
+          {/* Add other routes here */}
+        </Switch>
+      </Suspense>
       <Footer />
     </Router>
   );
