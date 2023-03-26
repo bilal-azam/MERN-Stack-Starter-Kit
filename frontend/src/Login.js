f1f1b656d8590aff0be6086014a70449cf20edfb
Bilal Azam <bilal.azam1@outlook.com>
2023-03-27 00:00:00 +0100

    Fix bugs identified during final testing

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,7 +1,39 @@
-import React from 'react';
+// frontend/src/Login.js (fix example)
+import React, { useState } from 'react';
+import { login } from './auth';
 
 function Login() {
-  return <header><h1>Login</h1></header>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await login({ email, password });
+    } catch (err) {
+      setError('Login failed');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button type="submit">Login</button>
+      {error && <p>{error}</p>}
+    </form>
+  );
 }
 
 export default Login;
